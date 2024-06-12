aee8f2d3072f2b918d70b53a5d7bc793d8f96e37
Krishna <pulluru.krishnareddy@mileseducation.com>
2024-06-12 23:48:24 +0530

    refactor: Update route path for mock test in App.tsx and Dashboard component

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/gret">
+    <BrowserRouter basename="/practice">
       <App />
       <ToastContainer />
     </BrowserRouter>
